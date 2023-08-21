5313df04d369c926c437cfb0ed25896e3d0d09d2
Sergey Bulgakov <sergeybulgakov93@mail.ru>
2023-08-21 21:25:53 +0300

    Changes

--- Snippets/settings.py
+++ Snippets/settings.py
@@ -125,3 +125,5 @@ STATICFILES_DIRS = [
   BASE_DIR / "static"
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -5,6 +5,6 @@ from MainApp import views
 
 urlpatterns = [
     path('', views.index_page),
-    path('snippets/add', views.add_snippet_page),
-    path('snippets/list', views.snippets_page),
+    path('snippets/add', views.add_snippet_page, name='add_snippet'),
+    path('snippets/list', views.snippets_page, name='snippets_list'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
