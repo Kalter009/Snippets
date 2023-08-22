15e4672bc834659ca072f68b97c90dd32e5cdadd
Vadim <vadimx30@gmail.com>
2023-08-22 19:21:41 +0300

    DONE M5 task12

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index_page, name='home'),
     path('snippets/add', views.add_snippet_page, name='snippets-add'),
     path('snippets/list', views.snippets_page, name='snippets-list'),
+    path('snippet/<int:snippet_id>', views.snippet_detail, name='snippet-detail'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
